132734fd5ba665c6b9d0f5ed6b07e37a8ee78fb2
Mr-Documents <ramzy.akwetey96@gmail.com>
2026-04-17 09:43:04 +0000

    Fixes

--- Backend/server.js
+++ Backend/server.js
@@ -47,4 +47,6 @@ app.use(express.json());
 app.use('/api', apiLimiter, climateRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => 
+  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`)
+);
